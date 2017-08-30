f2538b672bb668e08d3424c2a7c47347b7c6850f
kingdom <kingdom@DESKTOP-LVCFIJA>
2017-08-30 21:19:46 +0800

    修改JDK为1.7

--- pump-core-util/src/main/java/com/itouch8/pump/core/util/env/PumpVersion.java
+++ pump-core-util/src/main/java/com/itouch8/pump/core/util/env/PumpVersion.java
@@ -3,19 +3,16 @@ package com.itouch8.pump.core.util.env;
 import com.itouch8.pump.core.util.exception.Throw;
 import com.itouch8.pump.core.util.exception.meta.ExceptionCodes;
 
-
 public enum PumpVersion {
 
-    V_1_0_0("1.0.0", 1.8),
+    V_1_0_0("1.0.0", 1.7),
 
-    V_1_0_1("1.0.1", 1.8);
+    V_1_0_1("1.0.1", 1.7);
 
-    
     public static final PumpVersion Version = V_1_0_1;
 
-    
     private String version;
-    
+
     private double jdkVersion;
 
     private PumpVersion(String version, double jdkVersion) {
@@ -23,7 +20,6 @@ public enum PumpVersion {
         this.jdkVersion = jdkVersion;
     }
 
-    
     public static void checkJdkVersion() {
         if (EnvConsts.JDK_VERSION < Version.jdkVersion) {
             Throw.throwRuntimeException(ExceptionCodes.BF010004, EnvConsts.JDK_VERSION, Version.jdkVersion);
