a242c63791543db140819dd6c348b696c4a85ff9
kingdom <kingdom@DESKTOP-LVCFIJA>
2017-12-06 20:59:57 +0800

    增加HTTP工具

--- pump-util/src/main/java/com/itouch8/pump/util/http/HttpClientImpl.java
+++ pump-util/src/main/java/com/itouch8/pump/util/http/HttpClientImpl.java
@@ -1,7 +1,12 @@
 package com.itouch8.pump.util.http;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -12,6 +17,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
@@ -83,6 +89,83 @@ public class HttpClientImpl {
             e.printStackTrace();
         }
         return content;
+    }
+
+    public String post(String url, String data) {
+        //实例化httpClient  
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        //实例化post方法  
+        HttpPost httpPost = new HttpPost(url);
+        if (null != data) {
+            //解决中文乱码问题
+            StringEntity entity = new StringEntity(data, "utf-8");
+            entity.setContentEncoding("UTF-8");
+            entity.setContentType("application/json");
+            httpPost.setEntity(entity);
+        }
+        //结果  
+        CloseableHttpResponse response = null;
+        String content = "";
+        try {
+            //执行post方法  
+            response = httpclient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                content = EntityUtils.toString(response.getEntity(), "utf-8");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+    public String remoteImageToBase64(String imgUrl) {
+        URL url = null;
+        InputStream is = null;
+        ByteArrayOutputStream outStream = null;
+        HttpURLConnection httpUrl = null;
+        try {
+            url = new URL(imgUrl);
+            httpUrl = (HttpURLConnection) url.openConnection();
+            httpUrl.connect();
+            httpUrl.getInputStream();
+            is = httpUrl.getInputStream();
+
+            outStream = new ByteArrayOutputStream();
+            //创建一个Buffer字符串  
+            byte[] buffer = new byte[1024];
+            //每次读取的字符串长度，如果为-1，代表全部读取完毕  
+            int len = 0;
+            //使用一个输入流从buffer里把数据读取出来  
+            while ((len = is.read(buffer)) != -1) {
+                //用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度  
+                outStream.write(buffer, 0, len);
+            }
+            // 对字节数组Base64编码  
+            return new String(Base64.getEncoder().encode(outStream.toByteArray()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outStream != null) {
+                try {
+                    outStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (httpUrl != null) {
+                httpUrl.disconnect();
+            }
+        }
+        return imgUrl;
     }
 
 }
