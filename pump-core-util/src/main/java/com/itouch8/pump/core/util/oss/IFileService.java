7d4c8256057c0992939fe1d7a3363959c0fabaed
kingdom <kingdom@DESKTOP-LVCFIJA>
2018-03-17 20:16:27 +0800

    修改文件上传

--- pump-core-util/src/main/java/com/itouch8/pump/core/util/oss/IFileService.java
+++ pump-core-util/src/main/java/com/itouch8/pump/core/util/oss/IFileService.java
@@ -1,6 +1,7 @@
 package com.itouch8.pump.core.util.oss;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public interface IFileService {
 
@@ -9,6 +10,14 @@ public interface IFileService {
     public void send(String fileId, InputStream s, String contentType);
 
     public String get(String id);
+
+    /**
+     * 获取文件后保存到输出流
+     * 
+     * @param fileId
+     * @param os
+     */
+    public void get(String fileId, OutputStream os);
 
     public void del(String id);
 
